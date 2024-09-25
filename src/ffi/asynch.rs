3f55ee7da7439e5c197a76105469bdba655f4d3e
Factorial <27factorial@gmail.com>
2024-09-25 13:59:06 -0700

    update

--- src/ffi/asynch.rs
+++ src/ffi/asynch.rs
@@ -4,5 +4,7 @@ syscalls! {
     pub fn cs_acquire();
     pub fn cs_release();
     pub fn wait();
+    pub fn register_timer_wake(wake: extern "C" fn(), micros: u64);
+    pub fn register_io_wake(wake: extern "C" fn(), readable: bool, writable: bool);
     pub fn resume();
 }
